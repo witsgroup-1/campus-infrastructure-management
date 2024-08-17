cde7f929715bbf20717e702da406e3cb7b64a002
menzi-shazi <93788906+menzi-shazi@users.noreply.github.com>
2024-08-17 10:16:16 +0000

    fixed the button that fetches hello world from the api

--- api/index.js
+++ api/index.js
@@ -1,21 +1,8 @@
-const express = require('express');
-const path = require('https://campus-infrastructure-management.azurewebsites.net/api');
-const cors = require('cors'); // Import the cors package
-const app = express();
-const port = 3000;
+const indexRouter = require('express').Router()
+console.log("api/index.js")
+indexRouter.get('/', (request, response) => {
+    console.log("GET /api")
+    response.json({ message: 'Hello World' })
+})
 
-// Apply CORS middleware
-app.use(cors({
-    origin: 'https://campus-infrastructure-management.azurewebsites.net', // Replace with your frontend URL
-}));
-
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/api', (req, res) => {
-  res.send('hello world');
-});
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+module.exports = indexRouter
--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require("express");
 var app = express();
+const cors = require('cors')
+const indexRouter = require("./api/index");
 
 app.set("port", process.env.PORT || 3000);
 
 app.use(express.static("src"));
+app.use(cors())
+app.use("/api", indexRouter);
 
 app.listen(app.get("port"), function(){
-
     console.log("Server started on port "+app.get("port"));
 });
