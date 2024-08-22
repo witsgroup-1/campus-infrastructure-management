73e13af7a7c1231d541785d89a8250f40b495387
chloe900 <chloetdube@icloud.com>
2024-08-22 23:17:35 +0200

    created button components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.{html,js}', 
+    './ui-elements/**/*.{html,js}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'button-bg': '#1c44ac',
+        'button-border': '#b0b5fa',
+      },
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
