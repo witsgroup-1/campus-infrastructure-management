81a88fe87eac5c5d8ccd2fe17a566568b48d9a24
chloe900 <chloetdube@icloud.com>
2024-08-27 23:08:02 +0200

    small change

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -13,14 +13,11 @@ const firebaseConfig = {
   measurementId: process.env.APP_APP_MEASUREMENT_ID
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 //const analytics = getAnalytics(app);
-
-
-
 
 
 module.exports = { app, db, auth };
