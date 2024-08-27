95c2dd9be815a5b19f005898a55c112ad8c1a478
chloe900 <chloetdube@icloud.com>
2024-08-27 22:54:04 +0200

    temporary signup api created

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
+const { initializeApp } = require("firebase/app");
+const { getFirestore } = require("firebase/firestore");
+const { getAuth } = require("firebase/auth");
 
 const firebaseConfig = {
   apiKey: process.env.APP_API_KEY,
@@ -16,7 +16,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+//const analytics = getAnalytics(app);
 
 
 
--- src/signup.js
+++
@@ -1,2 +0,0 @@
-import {app, db, auth} from "./firebaseInit.js"
-
