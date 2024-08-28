314ad7de835ba55ace522f8bbea9801066920282
LunarrPhase <NewmanClaire6@gmail.com>
2024-08-28 21:31:14 +0200

    adding my api

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ var express = require("express");
 var app = express();
 const cors = require('cors')
 const indexRouter = require("./api/index");
+const signupRouter = require("./api/signup"); // Import the signup routes
+const maintenanceRouter = require('./api/maintenance');
+
 
 app.set("port", process.env.PORT || 3000);
 
 app.use(express.static("src"));
 app.use(cors())
 app.use("/api", indexRouter);
+app.use("/api", signupRouter); // Use the signup routes
+app.use("/api", maintenanceRouter);
 
 app.listen(app.get("port"), function(){
     console.log("Server started on port "+app.get("port"));
--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -2,6 +2,19 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+require('dotenv').config();
+const { getFirestore } = require("firebase/firestore");
+const { getAuth } = require("firebase/auth");
+
+// const firebaseConfig = {
+//   apiKey: process.env.APP_API_KEY,
+//   authDomain: process.env.APP_AUTH_DOMAIN,
+//   projectId: process.env.APP_PROJECT_ID,
+//   storageBucket: process.env.APP_STORAGE_BUCEKT,
+//   messagingSenderId: process.env.APP_MESSAGE_SENDER_ID,
+//   appId: process.env.APP_ID,
+//   measurementId: process.env.APP_APP_MEASUREMENT_ID
+// };
 
 const firebaseConfig = {
   apiKey: "AIzaSyCh1gI4eF7FbJ7wcFqFRzwSII-iOtNPMe0",
@@ -12,7 +25,6 @@ const firebaseConfig = {
   appId: "1:981921503275:web:78ce66a89f233a5c14f26e",
   measurementId: "G-Y95YE5ZDRY"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
