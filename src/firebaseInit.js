fe502999cc13fb6cc8b7016195d1ff786246f257
chloe900 <chloetdube@icloud.com>
2024-09-01 16:19:27 +0200

    use env variables, not raw init

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -3,7 +3,7 @@ const { initializeApp } = require("firebase/app");
 const { getFirestore } = require("firebase/firestore");
 const { getAuth, createUserWithEmailAndPassword } = require('firebase/auth');
 
-/*const firebaseConfig = {
+const firebaseConfig = {
   apiKey: process.env.APP_API_KEY,
   authDomain: process.env.APP_AUTH_DOMAIN,
   projectId: process.env.APP_PROJECT_ID,
@@ -11,16 +11,6 @@ const { getAuth, createUserWithEmailAndPassword } = require('firebase/auth');
   messagingSenderId: process.env.APP_MESSAGING_SENDER_ID,
   appId: process.env.APP_ID,
   measurementId: process.env.APP_MEASUREMENT_ID
-};*/
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCh1gI4eF7FbJ7wcFqFRzwSII-iOtNPMe0",
-  authDomain: "campusinfrastructuremanagement.firebaseapp.com",
-  projectId: "campusinfrastructuremanagement",
-  storageBucket: "campusinfrastructuremanagement.appspot.com",
-  messagingSenderId: "981921503275",
-  appId: "1:981921503275:web:78ce66a89f233a5c14f26e",
-  measurementId: "G-Y95YE5ZDRY"
 };
 
 const app = initializeApp(firebaseConfig);
