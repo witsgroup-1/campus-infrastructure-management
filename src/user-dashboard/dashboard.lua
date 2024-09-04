8e48efb9f95fb598c9f4a3d9896982856fde48ac
chloe900 <chloetdube@icloud.com>
2024-09-04 19:42:47 +0200

    claires ui
    claires ui

---
+++ src/user-dashboard/dashboard.lua
@@ -0,0 +1,19 @@
+<div class="MacbookPro161 w-[1760px] h-[1221px] relative bg-white flex-col justify-start items-start inline-flex">
+  <img class="WitsCentenaryLogoBlueOnWhiteColloquialLandscape2 w-[73.85px] h-[133.01px] origin-top-left rotate-[-0.58deg]" src="https://via.placeholder.com/74x133" />
+  <div class="Menu w-[78px] h-[79px] relative"></div>
+  <div class="Bell w-[51px] h-[70px] relative"></div>
+  <div class="InfrastructureManagement w-[656px] h-[174px] text-[#003b5c] text-5xl font-normal font-['Inter']">Infrastructure Management</div>
+  <div class="AvailableVenues w-[411px] h-[57px] text-center text-black text-4xl font-normal font-['Inter'] leading-none tracking-wide">Available venues...</div>
+  <img class="Rectangle14 w-[250px] h-[250px]" src="https://via.placeholder.com/250x250" />
+  <img class="Rectangle15 w-[250px] h-[250px]" src="https://via.placeholder.com/250x250" />
+  <img class="Rectangle19 w-[250px] h-[250px]" src="https://via.placeholder.com/250x250" />
+  <img class="Rectangle27 w-[250px] h-[250px]" src="https://via.placeholder.com/250x250" />
+  <img class="Rectangle28 w-[250px] h-[250px]" src="https://via.placeholder.com/250x250" />
+  <div class="ArrowBack w-[54px] h-[34px] relative origin-top-left -rotate-180"></div>
+  <div class="YourUpcomingBookings w-[703px] h-[54px] text-center text-black text-4xl font-normal font-['Inter'] leading-none tracking-wide">Your upcoming bookings...</div>
+  <div class="Rectangle16 w-[1144px] h-[115px] bg-[#d9d9d9] rounded-[30px]"></div>
+  <div class="Rectangle17 w-[1144px] h-[115px] bg-[#d9d9d9] rounded-[30px]"></div>
+  <div class="Rectangle18 w-[1144px] h-[115px] bg-[#d9d9d9] rounded-[30px]"></div>
+  <div class="Rectangle26 w-[212px] h-[99px] bg-[#917248] rounded-[50px]"></div>
+  <div class="Book w-[242px] h-[61px] text-center text-white text-[40px] font-bold font-['Inter'] leading-[18px] tracking-wide">Book</div>
+</div>
