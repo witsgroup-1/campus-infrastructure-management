b063a291286a37e73ceb11be3872b176e450a186
chloe900 <chloetdube@icloud.com>
2024-09-05 10:48:17 +0200

    added some details to userdashboard

--- src/user-dashboard/dashboard.js
+++ src/user-dashboard/dashboard.js
@@ -2,15 +2,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const menuIcon = document.getElementById('menu-icon');
     const sidebar = document.getElementById('sidebar');
     const closeBtn = document.getElementById('close-btn');
+    const notifBell = document.getElementById('notif-bell');
+    const notificationPanel = document.getElementById('notificationPanel');
+    const mainButton = document.getElementById('main-button');
+    const reportButton = document.getElementById('report-button');
+    const bookButton = document.getElementById('book-button');
+    
 
     const getSidebarWidth = () => {
         const screenWidth = window.innerWidth;
         if (screenWidth >= 1024) {
-            return '20%'; // For large screens (desktops)
+            return '20%'; //  (desktops)
         } else if (screenWidth >= 768) {
-            return '33%'; // For medium screens (tablets)
+            return '33%'; //(tablets)
         } else {
-            return '50%'; // For small screens (mobile)
+            return '50%'; //(mobile)
         }
     };
 
@@ -27,4 +33,32 @@ document.addEventListener('DOMContentLoaded', () => {
             sidebar.style.width = getSidebarWidth();
         }
     });
+
+    notifBell.addEventListener('click', () => {
+        notificationPanel.classList.toggle('hidden');
+    });
+
+    document.addEventListener('click', (event) => {
+        if (!notifBell.contains(event.target) && !notificationPanel.contains(event.target)) {
+            notificationPanel.classList.add('hidden');
+        }
+    });
+
+    let isExpanded = false;
+
+    mainButton.addEventListener('click', () => {
+        if (!isExpanded) {
+            reportButton.classList.remove('hidden');
+            bookButton.classList.remove('hidden');
+            isExpanded = true;
+        } else {
+            reportButton.classList.add('hidden');
+            bookButton.classList.add('hidden');
+            isExpanded = false;
+        }
+    });
+
+    reportButton.addEventListener('click', ()=>{
+        window.location.href = '../maintenance/maintenanceReports.html'
+    })
 });
