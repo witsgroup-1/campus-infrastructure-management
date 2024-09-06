5cce34d5c3d00683a671257b6c8b5da1422eec01
LunarrPhase <NewmanClaire6@gmail.com>
2024-09-06 22:25:09 +0200

    edited firebaseInit

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -5,17 +5,6 @@ const { getFirestore } = require("firebase/firestore");
 const { getAuth, createUserWithEmailAndPassword } = require('firebase/auth');
 
 
-// Initialize Firebase
-// const firebaseConfig = {
-
-//     apiKey: "AIzaSyCh1gI4eF7FbJ7wcFqFRzwSII-iOtNPMe0",
-//     authDomain: "campusinfrastructuremanagement.firebaseapp.com",
-//     projectId: "campusinfrastructuremanagement",
-//     storageBucket: "campusinfrastructuremanagement.appspot.com",
-//     messagingSenderId: "981921503275",
-//     appId: "1:981921503275:web:78ce66a89f233a5c14f26e",
-//     measurementId: "G-Y95YE5ZDRY"
-//   };
 
 const firebaseConfig = {
   apiKey: process.env.APP_API_KEY,
