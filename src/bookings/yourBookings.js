a3e6435f1a738c492e6b034ca390c682c1d92675
chloe900 <chloetdube@icloud.com>
2024-09-20 18:26:08 +0200

    viewing bookings

--- src/bookings/yourBookings.js
+++ src/bookings/yourBookings.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
-import { getFirestore, collection, query, where, getDocs } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
+import { getFirestore, collection, query, where, getDocs,getDoc,doc } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
 
 
 const firebaseConfig = {
@@ -13,7 +13,6 @@ const firebaseConfig = {
   measurementId: "G-Y95YE5ZDRY"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
@@ -21,26 +20,32 @@ const db = getFirestore(app);
 function showLoading() {
     const scheduledContent = document.getElementById('scheduled-content');
     scheduledContent.innerHTML = '<p>Loading bookings...</p>';
+    const pastContent = document.getElementById('in-progress-content');
+    pastContent.innerHTML = '<p>Loading bookings...</p>';
   }
 
-  function formatDate(date) {
-    const d = new Date(date);
-    const day = String(d.getDate()).padStart(2, '0');
-    const month = String(d.getMonth() + 1).padStart(2, '0');
-    const year = d.getFullYear().toString().slice(-2); // Get last 2 digits of year
-    return `${day}/${month}/${year}`;
+  function formatFirestoreTimestamp(timestamp) {
+    if (timestamp && timestamp.seconds) {
+      const date = new Date(timestamp.seconds * 1000);
+      const day = String(date.getDate()).padStart(2, '0');
+      const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+      const year = date.getFullYear().toString().slice(-2); // Get last 2 digits of year
+      return `${day}/${month}/${year}`; // Format as dd/mm/yy
+    }
+    return 'Invalid date';
   }
   
-  function formatTimeSlot(startTime, endTime) {
-    const start = new Date(startTime);
-    const end = new Date(endTime);
-    const formatTime = (date) => {
+  function formatTime(timestamp) {
+    if (timestamp && timestamp.seconds) {
+      const date = new Date(timestamp.seconds * 1000);
       const hours = String(date.getHours()).padStart(2, '0');
       const minutes = String(date.getMinutes()).padStart(2, '0');
-      return `${hours}:${minutes}`;
-    };
-    return `${formatTime(start)}-${formatTime(end)}`;
+      return `${hours}:${minutes}`; // Format as HH:MM
+    }
+    return 'Invalid time';
   }
+  
+
   
   async function fetchUserBookings(userId) {
     try {
@@ -68,7 +73,6 @@ function showLoading() {
 
   async function getFirestoreUserIdByEmail(email) {
     try {
-      // Query the Firestore 'users' collection where the 'email' field equals the Firebase Auth email
       const usersRef = collection(db, 'users');
       const q = query(usersRef, where('email', '==', email));
       const querySnapshot = await getDocs(q);
@@ -88,21 +92,16 @@ function showLoading() {
   }
 
 
-  // Function to hide the loading state
-function hideLoading() {
-  const bookingsContainer = document.getElementById('bookings-container');
-  bookingsContainer.innerHTML = ''; // Clear the loading message
-}
   
 let currentPageUpcomingDesktop = 1;
 let currentPagePastDesktop = 1;
 let currentPageUpcomingMobile = 1;
 let currentPagePastMobile = 1;
 
-const itemsPerPageDesktop = 5;
+const itemsPerPageDesktop = 4;
 const itemsPerPageMobile = 2;
 
-function displayBookings(bookings) {
+async function displayBookings(bookings, userId) {
   const scheduledContent = document.getElementById('scheduled-content');
   const pastContent = document.getElementById('in-progress-content');
   const mobileScheduledContent = document.getElementById('mobile-scheduled-content');
@@ -116,111 +115,198 @@ function displayBookings(bookings) {
 
   const now = new Date();
 
-  // Separate upcoming and past bookings
-  const upcomingBookings = bookings.filter(booking => new Date(booking.start_time) > now);
-  const pastBookings = bookings.filter(booking => new Date(booking.start_time) <= now);
+  const upcomingBookings = bookings.filter(booking => {
+    const startTime = booking.startTime ? new Date(booking.startTime.seconds * 1000) : null;
+    return startTime && startTime > now;
+  }).sort((a, b) => {
+    const startA = new Date(a.startTime.seconds * 1000);
+    const startB = new Date(b.startTime.seconds * 1000);
+    return startA - startB;
+  });
 
+  const pastBookings = bookings.filter(booking => {
+    const startTime = booking.startTime ? new Date(booking.startTime.seconds * 1000) : null;
+    return startTime && startTime <= now;
+  }).sort((a, b) => {
+    const startA = new Date(a.startTime.seconds * 1000);
+    const startB = new Date(b.startTime.seconds * 1000);
+    return startA - startB;
+  });
 
   const paginatedUpcomingDesktop = paginateBookings(upcomingBookings, currentPageUpcomingDesktop, itemsPerPageDesktop);
   const paginatedPastDesktop = paginateBookings(pastBookings, currentPagePastDesktop, itemsPerPageDesktop);
-
   
   const paginatedUpcomingMobile = paginateBookings(upcomingBookings, currentPageUpcomingMobile, itemsPerPageMobile);
   const paginatedPastMobile = paginateBookings(pastBookings, currentPagePastMobile, itemsPerPageMobile);
 
- 
-  renderDesktopBookings(paginatedUpcomingDesktop, scheduledContent, 'upcoming');
-  renderDesktopBookings(paginatedPastDesktop, pastContent, 'past');
-
- 
-  renderMobileBookings(paginatedUpcomingMobile, mobileScheduledContent, 'upcoming');
-  renderMobileBookings(paginatedPastMobile, mobilePastContent, 'past');
-
   
+  renderDesktopBookings(paginatedUpcomingDesktop, scheduledContent, 'upcoming', userId);
+  renderDesktopBookings(paginatedPastDesktop, pastContent, 'past', userId);
+
+  renderMobileBookings(paginatedUpcomingMobile, mobileScheduledContent, 'upcoming', userId);
+  renderMobileBookings(paginatedPastMobile, mobilePastContent, 'past', userId);
+
   renderPaginationControls(upcomingBookings, currentPageUpcomingDesktop, itemsPerPageDesktop, scheduledContent, 'desktop', 'upcoming');
   renderPaginationControls(pastBookings, currentPagePastDesktop, itemsPerPageDesktop, pastContent, 'desktop', 'past');
-
- 
   renderPaginationControls(upcomingBookings, currentPageUpcomingMobile, itemsPerPageMobile, mobileScheduledContent, 'mobile', 'upcoming');
   renderPaginationControls(pastBookings, currentPagePastMobile, itemsPerPageMobile, mobilePastContent, 'mobile', 'past');
 }
-
 function paginateBookings(bookings, page, itemsPerPage) {
   const start = (page - 1) * itemsPerPage;
   const end = start + itemsPerPage;
   return bookings.slice(start, end);
 }
 
-function renderDesktopBookings(bookings, container, type) {
+ window.cancelBooking = async function(userId, bookingId) {
+  try {
+    const url = `https://campus-infrastructure-management.azurewebsites.net/api/users/${userId}/bookings/${bookingId}`;
+    const response = await fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'x-api-key': 'QGbXcci4doXiHamDEsL0cBLjXNZYGCmBUmjBpFiITsNTLqFJATBYWGxKGzpxhd00D5POPOlePixFSKkl5jXfScT0AD6EdXm6TY0mLz5gyGXCbvlC5Sv7SEWh7QO6PewW',
+        'Content-Type': 'application/json'
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to delete booking, please try again.');
+    }
+
+    alert(`Booking with ID ${bookingId} cancelled successfully.`);
+  } catch (error) {
+    console.error('Error cancelling booking:', error);
+    alert('Error cancelling booking: ' + error.message);
+  }
+}
+
+window.bookAgain = async function(userId, venueId) {
+
+  try {
+    const url = `https://campus-infrastructure-management.azurewebsites.net/api/users/${userId}/bookings/${bookingId}`;
+    const response = await fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'x-api-key': 'QGbXcci4doXiHamDEsL0cBLjXNZYGCmBUmjBpFiITsNTLqFJATBYWGxKGzpxhd00D5POPOlePixFSKkl5jXfScT0AD6EdXm6TY0mLz5gyGXCbvlC5Sv7SEWh7QO6PewW',
+        'Content-Type': 'application/json'
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to delete booking, please try again.');
+    }
+
+    alert(`Booking with ID ${bookingId} cancelled successfully.`);
+  } catch (error) {
+    console.error('Error cancelling booking:', error);
+    alert('Error cancelling booking: ' + error.message);
+  }
+
+
+  
+}
+
+async function renderDesktopBookings(bookings, container, type, userId) {
   const bookingsSection = document.createElement('div');
   bookingsSection.classList.add(`${type}-bookings`, 'mb-4');
-  
+
   if (bookings.length === 0) {
     bookingsSection.innerHTML = `<p>No ${type} bookings.</p>`;
   } else {
-    bookings.forEach(booking => {
+    for (const booking of bookings) {
+      const venueDoc = await fetchVenueName(booking.venueId);
+      const venueName = venueDoc.exists() ? venueDoc.data().Name : 'Venue not found';
+      
+      const formattedStartTime = formatFirestoreTimestamp(booking.startTime);
+      const formattedStartHour = formatTime(booking.startTime);
+      const formattedEndHour = formatTime(booking.endTime);
+
       const bookingElement = document.createElement('div');
       bookingElement.classList.add('w-full', 'bg-gray-200', 'rounded-lg', 'mb-2', 'p-4', 'shadow-sm');
+
       bookingElement.innerHTML = `
         <div class="flex justify-between items-center">
           <div>
-            <div><strong>Name:</strong> ${booking.name}</div>
-            <div><strong>Venue:</strong> ${booking.venue_name}</div>
-            <div><strong>Date:</strong> ${formatDate(booking.start_time)}</div>
-            <div><strong>Slot:</strong> ${formatTimeSlot(booking.start_time, booking.end_time)}</div>
+            <div><strong>Purpose:</strong> ${booking.purpose}</div>
+            <div><strong>Venue:</strong> ${venueName}</div>
+            <div><strong>Date:</strong> ${formattedStartTime}</div>
+            <div><strong>Slot:</strong> ${formattedStartHour} - ${formattedEndHour}</div>
           </div>
           ${type === 'upcoming' 
-            ? `<button class="bg-red-500 text-white px-3 py-1 rounded" onclick="cancelBooking('${booking.id}')">Cancel</button>`
+            ? `<button class="bg-red-500 text-white px-3 py-1 rounded" onclick="cancelBooking('${userId}', '${booking.id}')">Cancel</button>`
             : `<button class="bg-green-500 text-white px-3 py-1 rounded" onclick="bookAgain('${booking.id}')">Book Again</button>`}
         </div>
       `;
+      
       bookingsSection.appendChild(bookingElement);
-    });
+    }
   }
 
   container.appendChild(bookingsSection);
 }
 
-function renderMobileBookings(bookings, container, type) {
+
+
+async function renderMobileBookings(bookings, container, type, userId) {
   const bookingsSection = document.createElement('div');
-  bookingsSection.classList.add(`${type}-bookings-mobile`, 'mb-4');
-  
+  bookingsSection.classList.add(`${type}-bookings`, 'mb-4');
+
   if (bookings.length === 0) {
     bookingsSection.innerHTML = `<p>No ${type} bookings.</p>`;
   } else {
-    bookings.forEach(booking => {
+    for (const booking of bookings) {
+      const venueDoc = await fetchVenueName(booking.venueId);
+      const venueName = venueDoc.exists() ? venueDoc.data().Name : 'Venue not found';
+      
+      const formattedStartTime = formatFirestoreTimestamp(booking.startTime);
+      const formattedStartHour = formatTime(booking.startTime);
+      const formattedEndHour = formatTime(booking.endTime);
+
       const bookingElement = document.createElement('div');
       bookingElement.classList.add('w-full', 'bg-gray-200', 'rounded-lg', 'mb-2', 'p-4', 'shadow-sm');
+
       bookingElement.innerHTML = `
-        <div class="flex flex-col justify-between items-start">
+        <div class="flex justify-between items-center">
           <div>
-            <div><strong>Name:</strong> ${booking.name}</div>
-            <div><strong>Venue:</strong> ${booking.venue_name}</div>
-            <div><strong>Date:</strong> ${formatDate(booking.start_time)}</div>
-            <div><strong>Slot:</strong> ${formatTimeSlot(booking.start_time, booking.end_time)}</div>
+            <div><strong>Purpose:</strong> ${booking.purpose}</div>
+            <div><strong>Venue:</strong> ${venueName}</div>
+            <div><strong>Date:</strong> ${formattedStartTime}</div>
+            <div><strong>Slot:</strong> ${formattedStartHour} - ${formattedEndHour}</div>
           </div>
           ${type === 'upcoming' 
-            ? `<button class="bg-red-500 text-white w-full mt-2 rounded" onclick="cancelBooking('${booking.id}')">Cancel</button>`
-            : `<button class="bg-green-500 text-white w-full mt-2 rounded" onclick="bookAgain('${booking.id}')">Book Again</button>`}
+            ? `<button class="bg-red-500 text-white px-3 py-1 rounded" onclick="cancelBooking('${userId}', '${booking.id}')">Cancel</button>`
+            : `<button class="bg-green-500 text-white px-3 py-1 rounded" onclick="bookAgain('${booking.id}')">Book Again</button>`}
         </div>
       `;
+      
       bookingsSection.appendChild(bookingElement);
-    });
+    }
   }
 
   container.appendChild(bookingsSection);
+}
+
+async function fetchVenueName(venueId) {
+  try {
+    const venueRef = doc(db, 'venues', venueId);  
+    const venueDoc = await getDoc(venueRef); 
+    return venueDoc;
+    
+  } catch (error) {
+    console.error('Error fetching venue:', error);
+    return null;  // Handle error case
+  }
 }
 
 function renderPaginationControls(bookings, currentPage, itemsPerPage, container, deviceType, bookingType) {
   const totalPages = Math.ceil(bookings.length / itemsPerPage);
   
-  if (totalPages <= 1) return; // No pagination needed if there's only one page
+  if (totalPages <= 1) return;
 
   const paginationControls = document.createElement('div');
   paginationControls.classList.add('pagination-controls', 'mt-4', 'flex', 'justify-between');
 
   const prevButton = document.createElement('button');
-  prevButton.classList.add('bg-gray-400', 'text-white', 'px-4', 'py-2', 'rounded');
+  prevButton.classList.add('bg-white', 'text-[#917248]', 'px-4', 'py-2', 'rounded');
   prevButton.innerText = 'Previous';
   prevButton.disabled = currentPage === 1;
   prevButton.onclick = () => {
@@ -235,7 +321,7 @@ function renderPaginationControls(bookings, currentPage, itemsPerPage, container
   };
 
   const nextButton = document.createElement('button');
-  nextButton.classList.add('bg-gray-400', 'text-white', 'px-4', 'py-2', 'rounded');
+  nextButton.classList.add('bg-white', 'text-[#917248]', 'px-4', 'py-2', 'rounded');
   nextButton.innerText = 'Next';
   nextButton.disabled = currentPage === totalPages;
   nextButton.onclick = () => {
@@ -256,36 +342,20 @@ function renderPaginationControls(bookings, currentPage, itemsPerPage, container
 }
 
 
-  
-  // Function to handle booking cancellation
-  function cancelBooking(bookingId) {
-    alert(`Booking with ID ${bookingId} cancelled (You would add logic to delete the booking).`);
-    // Add logic to delete the booking from the server
-  }
-  
-  // Function to handle rebooking
-  function bookAgain(bookingId) {
-    alert(`Booking with ID ${bookingId} being booked again (You would add logic to create a new booking).`);
-    // Add logic to create a new booking based on the past booking
-  }
-  
-
   async function loadUserBookings(userEmail) {
-    showLoading(); // Show loading message
+    showLoading();
   
-    // Fetch the corresponding Firestore userId using Firebase Auth email
     const firestoreUserId = await getFirestoreUserIdByEmail(userEmail);
-    console.log(firestoreUserId)
+    console.log(firestoreUserId);
   
     if (firestoreUserId) {
-      // Use the Firestore userId to fetch bookings
       const bookings = await fetchUserBookings(firestoreUserId);
-      displayBookings(bookings);
+      console.log(bookings);
+      // Pass firestoreUserId to displayBookings
+      displayBookings(bookings, firestoreUserId);
     } else {
       console.error('Could not find Firestore userId for the given email.');
     }
-  
-    hideLoading(); // Hide loading message
   }
   
   onAuthStateChanged(auth, (user) => {
