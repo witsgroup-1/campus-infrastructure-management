246b8fe03e0f563cfe92207dbfdc6d24fc0110cd
dinnycodes <99383868+dinnycodes@users.noreply.github.com>
2024-10-24 17:13:55 +0200

    add features of venue to booking page

--- src/make-booking/book-venue.js
+++ src/make-booking/book-venue.js
@@ -2,7 +2,6 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
 
 // Firebase configuration
 const firebaseConfig = {
@@ -18,7 +17,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app);
 let venues = []; // Declare venues as a global variable
 
 const API_KEY = "QGbXcci4doXiHamDEsL0cBLjXNZYGCmBUmjBpFiITsNTLqFJATBYWGxKGzpxhd00D5POPOlePixFSKkl5jXfScT0AD6EdXm6TY0mLz5gyGXCbvlC5Sv7SEWh7QO6PewW";
@@ -196,6 +194,7 @@ async function renderVenues(venues, userData) {
                     <h2 class="text-lg font-semibold">${venue.Name || 'Unknown Name'}</h2>
                     <p class="text-sm text-gray-600">Category: ${venue.Category || 'Unknown Category'}</p>
                     <p class="text-sm text-gray-600">Capacity: ${venue.Capacity || 'Unknown Capacity'}</p>
+                     <p class="text-sm text-gray-600">Features: ${venue.Features || 'Unknown Features'}</p>
                     <p class="text-red-500">This venue is currently under maintenance.</p>
                 </div>
             `;
@@ -205,6 +204,7 @@ async function renderVenues(venues, userData) {
                     <h2 class="text-lg font-semibold">${venue.Name || 'Unknown Name'}</h2>
                     <p class="text-sm text-gray-600">Category: ${venue.Category || 'Unknown Category'}</p>
                     <p class="text-sm text-gray-600">Capacity: ${venue.Capacity || 'Unknown Capacity'}</p>
+                     <p class="text-sm text-gray-600">Features: ${venue.Features || 'Unknown Features'}</p>
                 </div>
                 <button class="bg-[#917248] text-white px-3 py-1 rounded hover:bg-blue-600 focus:outline-none" onclick="window.location.href='booking-details.html?bookingId=${venue.id}'">Book</button>
             `;
