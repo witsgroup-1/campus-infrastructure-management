10ef8487ed1e56e38b1e15dc0298cd376d5800bf
MayTheSlay <2543986@students.wits.ac.za>
2024-10-25 01:52:40 +0200

    updated security details

--- src/user-dashboard/dashboard.js
+++ src/user-dashboard/dashboard.js
@@ -273,7 +273,10 @@ const securityUrl = 'https://campus-infrastructure-management.azurewebsites.net/
 const ourSecurityUrl = `https://campus-infrastructure-management.azurewebsites.net/api/securityInfo`;
 
 async function fetchSecurityContact() {
+    const loading = document.getElementById('loading');
     try {
+
+        loading.style.display = 'block';
         const response = await fetch(securityUrl, {
             method: 'GET',
            // mode: 'no-cors',
@@ -316,6 +319,9 @@ async function fetchSecurityContact() {
     } catch (err) {
         console.error('Error fetching security contact information:', err);
         fetchOurSecurityContact();
+    }
+    finally {
+        loading.style.display = 'none';
     }
 }
 
